6d0bb61ddb78f1ae462e561b2efd7ce96598bbc5
Maya Montague <mayamontague4@gmail.com>
2024-08-28 10:51:25 -0500

    .

---
+++ inrapp/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace inrapp
+{
+    
+    public class Pair
+    {
+        public int Key;
+        public string Value;
+
+        public Pair(int key, string value)
+        {
+            Key = key;
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return $"({Key}, {Value})";
+        }
+    }
+
+    public class Solution
+    {
+        
+        public List<List<Pair>> InsertionSort(List<Pair> pairs)
+        {
+            int n = pairs.Count;
+            List<List<Pair>> res = new List<List<Pair>>();  
+
+            
+            for (int i = 1; i < n; i++)
+            {
+                Pair keyPair = pairs[i]; 
+                int j = i - 1;
+
+                
+                while (j >= 0 && pairs[j].Key > keyPair.Key)
+                {
+                    pairs[j + 1] = pairs[j];
+                    j--;
+                }
+
+                
+                pairs[j + 1] = keyPair;
+
+                
+                List<Pair> cloneList = new List<Pair>(pairs);
+                res.Add(cloneList);
+            }
+            return res;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            List<Pair> pairs = new List<Pair>
+            {
+                new Pair(8, "Vanilla"),
+                new Pair(4, "Chocolate"),
+                new Pair(10, "Mocha"),
+                new Pair(3, "Rocky Road"),
+                new Pair(11, "Cookies n Cream")
+            };
+
+            Solution solution = new Solution();
+            List<List<Pair>> sortedStates = solution.InsertionSort(pairs);
+
+            
+            Console.WriteLine("List after each insertion:");
+            foreach (var state in sortedStates)
+            {
+                Console.WriteLine(string.Join(", ", state));
+            }
+        }
+    }
+}
---
+++ reverseapp/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace reverseapp
+{
+    class Program
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+
+        public class Solution
+        {
+            public ListNode ReverseList(ListNode head)
+            {
+                ListNode prev = null;
+                ListNode current = head;
+                ListNode nextNode = null;
+
+                while (current != null)
+                {
+                    nextNode = current.next;
+                    current.next = prev;
+                    prev = current;
+                    current = nextNode;
+                }
+
+                return prev;
+            }
+        }
+
+        static void Main(string[] args)
+         {
+            
+            ListNode head = new ListNode(15, new ListNode(30, new ListNode(45, new ListNode(60, new ListNode(75)))));
+
+            Solution solution = new Solution();
+            ListNode reversedHead = solution.ReverseList(head);
+
+            PrintList(reversedHead);
+        }
+
+        static void PrintList(ListNode node)
+        {
+            while (node != null)
+            {
+                Console.Write(node.val + " ");
+                node = node.next;
+            }
+            Console.WriteLine();
+        }
+    }
+}    
